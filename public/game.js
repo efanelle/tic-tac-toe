82066a0d7cd8e6743dfa6eda15499470f375cd2d
Erica Fanelle <efanelle@gmail.com>
2017-04-28 11:53:18 -0400

    Add most functionality

--- public/game.js
+++ public/game.js
@@ -1,18 +1,24 @@
 // $(document).ready(function(){
 // })
 
-var squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+//board square options
 var X = 'X';
 var O = 'O';
 var empty = ' ';
+
+var squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+var combos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], 
+[2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 var selected = [];
+var moves1 = [];
+var moves2 = [];
 var move = true;
-var player1, player2;
+var player1 = 'Player 1';
+var player2 = 'Player 2';
 
 //initialize board with empty blocks 
 squares.map(function(el) {
   $(`#${el}`).append(`<p>${empty}</p>`);
-  // $(`#${el}`).append(`<p>${el.toString()}</p>`);
 })
 
 function enterPlayers() {
@@ -38,16 +44,19 @@ $('.user').keyup(function(e) {
 
 $('.square').on('click', function(e) {
 
-  if(selected.indexOf(this.id) < 0){
-    selected.push(this.id);
+  if (moves1.indexOf(this.id) < 0 && moves2.indexOf(this.id) < 0) {
     if(move) {
+      moves1.push(this.id)
       $(this).text(X).addClass('selected')
       $('.player2').addClass('active')
       $('.player1').removeClass('active')
+      checkWinner(player1);
     } else {
+      moves2.push(this.id)
       $(this).text(O).addClass('selected')
       $('.player2').removeClass('active')
       $('.player1').addClass('active')
+      checkWinner(player2);
     }
   }
   move = !move;
@@ -55,11 +64,34 @@ $('.square').on('click', function(e) {
 
 function reset() {
   squares.map(function(square) {
-    $(`#${square}`).text(`${empty}`).removeClass('selected');
+    $(`#${square}`).text(`${empty}`).removeClass('selected winner');
   }) 
-  selected = [];
-  $('.player1').removeClass('active')
+  selected = [], moves1 = [], moves2 = [];
+  $('.player1').addClass('active')
   $('.player2').removeClass('active')
   console.log('resetting...')
   $('#users').show()
+  move = true;
 }
+
+function checkWinner (player) {
+  for (var i = 0; i < combos.length; i++) {
+    var win = combos[i];
+    var matchX = win.every(function(el) {
+      return $('#' + el).text() === X
+    })
+    var matchO = win.every(function(el) {
+      return $('#' + el).text() === O
+    })
+    if(matchX || matchO) {
+      console.log(player)
+      alert (`${player} is the winner!`)
+      win.map(function(el) {
+        $(`#${el}`).addClass('winner');
+      })
+    }
+
+  }
+
+}
+
